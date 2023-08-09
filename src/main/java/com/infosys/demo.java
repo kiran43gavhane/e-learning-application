c9ee51ab9722ca8ef937c7539e192cb9ae4689d3
Kiran Gavhane <gavhanekiran43@gmail.com>
2023-08-09 20:37:38 +0530

    logic for dividing added

--- src/main/java/com/infosys/demo.java
+++ src/main/java/com/infosys/demo.java
@@ -5,5 +5,12 @@ public class demo {
 	public static void main(String[] args) {
 	
 		System.out.println("hiiiii");
+		
+		
+		// logic of dividing
+		boolean d = true;
+		long t = 3244;
+		
+		
 	}
 }
