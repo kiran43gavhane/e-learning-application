211e405fc494a905452efb5540804bca354d3a44
Kiran Gavhane <gavhanekiran43@gmail.com>
2023-08-09 20:14:14 +0530

    delete the logic in demo class

--- src/main/java/com/infosys/demo.java
+++ src/main/java/com/infosys/demo.java
@@ -3,18 +3,7 @@ package com.infosys;
 public class demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hare krishna prabhu ji");
-		
-		
-		// bug - 122 fixed
-		System.out.println("bug 122 fixed");
-		
-		// fixing bug 121 (pending)
-		int a=12;
-		int b=34; 
-		int c=a+b;
-		System.out.println(c);
+	
 		
  
 	}
